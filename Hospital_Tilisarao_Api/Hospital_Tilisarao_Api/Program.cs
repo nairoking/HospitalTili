38655164ad253665df310419173c154315fbd958
nairoking <89227727+nairoking@users.noreply.github.com>
2022-06-04 18:38:49 -0300

    Initial commit

---
+++ Hospital_Tilisarao_Api/Hospital_Tilisarao_Api/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital_Tilisarao_Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			CreateWebHostBuilder(args).Build().Run();
+        }
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		{
+			var host = WebHost.CreateDefaultBuilder(args)
+				.ConfigureLogging(logging =>
+				{
+					logging.ClearProviders();//limpia los proveedores x defecto de log (consola+depuración)
+					logging.AddConsole();//agrega log de consola
+										 //logging.AddConfigur(new LoggerConfiguration().WriteTo.File("serilog.txt").CreateLogger())
+				})
+				.UseStartup<Startup>();
+			return host;
+		}
+
+		public static IWebHostBuilder CreateKestrel(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.Build();
+			var host = new WebHostBuilder()
+				.UseConfiguration(config)
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				//.UseUrls("http://localhost:5000", "https://localhost:5001")//permite escuchar SOLO peticiones locales
+				.UseUrls("http://*:5000", "https://*:5001")//permite escuchar peticiones locales y remotas
+				.UseIISIntegration()
+				.UseStartup<Startup>();
+			return host;
+		}
+
+	}
+}
---
+++ Hospital_Tilisarao_Api/Hospital_Tilisarao_Api/Startup.cs
@@ -0,0 +1,123 @@
+using Hospital_Tilisarao_Api.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Hospital_Tilisarao_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+               .AddCookie(options =>//el sitio web valida con cookie
+               {
+                   options.LoginPath = "/Home/Index";
+                   options.LogoutPath = "/Usuarios/Logout";
+                   options.AccessDeniedPath = "/Home/Restringido";
+               })
+               .AddJwtBearer(options =>//la api web valida con token
+               {
+                   options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidateAudience = true,
+                       ValidateLifetime = true,
+                       ValidateIssuerSigningKey = true,
+                       ValidIssuer = Configuration["TokenAuthentication:Issuer"],
+                       ValidAudience = Configuration["TokenAuthentication:Audience"],
+                       IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(
+                           Configuration["TokenAuthentication:SecretKey"])),
+                   };
+                   // opción extra para usar el token el hub
+                   options.Events = new JwtBearerEvents
+                   {
+                       OnMessageReceived = context =>
+                       {
+                           // Read the token out of the query string
+                           var accessToken = context.Request.Query["access_token"];
+                           // If the request is for our hub...
+                           var path = context.HttpContext.Request.Path;
+                           if (!string.IsNullOrEmpty(accessToken) &&
+                               path.StartsWithSegments("/chatsegurohub"))
+                           {//reemplazar la url por la usada en la ruta ?
+                               context.Token = accessToken;
+                           }
+                           return Task.CompletedTask;
+                       }
+                   };
+               })
+
+
+               ;
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Empleado", policy => policy.RequireClaim(ClaimTypes.Role, "Administrador", "Empleado"));
+                options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador", "SuperAdministrador"));
+            });
+            services.AddMvc();
+            services.AddSignalR();
+            services.AddDbContext<DataContext>(
+                options => options.UseSqlServer(
+                    Configuration["ConnectionStrings:DefaultConnection"]
+                )
+            );
+            services.AddHttpContextAccessor();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCookiePolicy(new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.None,
+            });
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
